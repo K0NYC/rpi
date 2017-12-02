c231e85a1d0ae5882a12e4da24996c2707e1f2f6
seledkinpylesos <32525647+seledkinpylesos@users.noreply.github.com>
2017-12-01 22:15:52 -0500

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -2,6 +2,7 @@
 
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+import logging
 
 # Define GPIO mode
 GPIO.setmode(GPIO.BOARD)
@@ -11,18 +12,19 @@ relay = 26 #GPIO 7
 GPIO.setwarnings (False)
 GPIO.setup(relay, GPIO.OUT)
 
+logging.basicConfig(filename='/var/log/messages',level=logging.INFO)
 
 # This is the Subscriber
 
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-	client.subscribe("/livingroom/AC/power")
+        logging.info("Connected to broker with result code "+str(rc))
+        client.subscribe("/livingroom/AC/power")
 
 def on_message(client, userdata, msg):
-	if msg.payload.decode() == "on":
-		GPIO.output(relay, True)
-	elif msg.payload.decode() == "off":
-		GPIO.output(relay, False)
+        if msg.payload.decode() == "on":
+                GPIO.output(relay, True)
+        elif msg.payload.decode() == "off":
+                GPIO.output(relay, False)
 
 
 client = mqtt.Client()
