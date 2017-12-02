84240a04f97470b11486adb85697794a79d025f1
seledkinpylesos <32525647+seledkinpylesos@users.noreply.github.com>
2017-12-01 22:09:06 -0500

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -3,16 +3,18 @@
 import paho.mqtt.client as mqtt
 from w1thermsensor import W1ThermSensor
 from  time import sleep
+import logging
+
+logging.basicConfig(filename='/var/log/messages',level=logging.INFO)
 
 sensor = W1ThermSensor()
-temperature_in_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
 
 # This is the Publisher
 client = mqtt.Client()
 client.connect("192.168.10.8",1883,60)
+logging.info('Publisher started')
 
 while True:
-    client.publish("/livingroom/AC/temp", temperature_in_fahrenheit);
+    client.publish("/livingroom/AC/temp", sensor.get_temperature(W1ThermSensor.DEGREES_F));
     sleep(3)
-
-
+    
